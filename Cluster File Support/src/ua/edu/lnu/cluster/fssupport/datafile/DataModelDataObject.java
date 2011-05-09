102ec7f5365b1c3ddfd5bcc8b122a2f8ac8c5057
pif <pif@pif>
2011-05-09 20:07:00 +0300

    dataobject for datamodel

--- Cluster File Support/src/ua/edu/lnu/cluster/fssupport/datafile/DataModelDataObject.java
+++ Cluster File Support/src/ua/edu/lnu/cluster/fssupport/datafile/DataModelDataObject.java
@@ -4,10 +4,16 @@
  */
 package ua.edu.lnu.cluster.fssupport.datafile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import org.openide.cookies.OpenCookie;
+import org.openide.cookies.SaveCookie;
+import org.openide.filesystems.FileChooserBuilder;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataNode;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
@@ -27,18 +33,18 @@ public class DataModelDataObject extends MultiDataObject {
     private DataModel model;
     private FileObject realFile;
     private CookieSet cookies;
-    private OpenDataModelAction openAction;
 
     public DataModelDataObject(FileObject fileObject, MultiFileLoader loader) throws DataObjectExistsException, IOException {
         super(fileObject, loader);
         realFile = fileObject;
         model = readFile(fileObject);
-        
+
         cookies = getCookieSet();
         cookies.assign(DataModel.class, model);
-        openAction = new OpenDataModelAction(getPrimaryEntry());
 
-        cookies.add((Node.Cookie) openAction);
+        cookies.add((Node.Cookie) new OpenDataModelAction(getPrimaryEntry()));
+        cookies.add((Node.Cookie) new SaveDataModelAction(this));
+        
         cookies.add(this);
     }
 
@@ -47,7 +53,7 @@ public class DataModelDataObject extends MultiDataObject {
     protected Node createNodeDelegate() {
         // can use DataNode here as well
         DataNode node = new DataNode(this, Children.LEAF, getLookup());
-       // DataNode node = new DataNode(this, Children.LEAF, cookies.getLookup());
+        // DataNode node = new DataNode(this, Children.LEAF, cookies.getLookup());
         //  node.setShortDescription("Name is " + getLookup().lookup(NeuralNetwork.class).toString());
         node.setDisplayName(realFile.getName());
 
@@ -82,28 +88,77 @@ public class DataModelDataObject extends MultiDataObject {
         return null;
     }
 
-    private static class OpenDataModelAction  extends OpenSupport implements OpenCookie{
+    private static class OpenDataModelAction extends OpenSupport implements OpenCookie {
 
         private DataModelDataObject dataObject;
-        
+
         public OpenDataModelAction(DataModelDataObject.Entry primaryEntry) {
             super(primaryEntry);
             dataObject = (DataModelDataObject) primaryEntry.getDataObject();
         }
-        
-            @Override
-    public void open() {
-        // check if it is allready loaded before loading
-        // nnDataObject.loadObject();
-        DataModel nnet = dataObject.getDataModel();
 
-        TCManager.getInstance().openDataModelWindow(nnet);
-    }
+        @Override
+        public void open() {
+            DataModel model = dataObject.getDataModel();
+
+            TCManager.getInstance().openDataModelWindow(model);
+        }
 
         @Override
         protected CloneableTopComponent createCloneableTopComponent() {
             throw new UnsupportedOperationException("Not supported yet.");
         }
-
     }
+    
+    private static final class SaveDataModelAction implements SaveCookie {
+
+        private DataModelDataObject dataObject;
+
+        public SaveDataModelAction(DataModelDataObject dataObject) {
+            this.dataObject = dataObject;
+        }
+        
+        @Override
+        public void save() throws IOException {
+            if (dataObject != null) {
+                File saveTo = FileUtil.toFile(dataObject.getPrimaryFile());
+                save(saveTo);
+            } else {
+                saveAs();
+            }
+        }
+
+        public void saveAs() throws IOException {
+            File f = new FileChooserBuilder(SaveDataModelAction.class).setTitle("Save file").showSaveDialog();
+            if (f != null) {
+                if (!f.getAbsolutePath().endsWith(".cluster")) {
+                    f = new File(f.getAbsolutePath() + ".cluster");
+                }
+                try {
+                    if (!f.exists()) {
+                        if (!f.createNewFile()) {
+                            // TODO fail message!
+                            return;
+                        }
+                    } else {
+                        // TODO overwrite?
+                    }
+                    save(f.getAbsoluteFile());
+                } catch (IOException ioe) {
+                    Exceptions.printStackTrace(ioe);
+                }
+            }
+        }
+
+        private void save(File f) throws IOException {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(f));
+            try {
+                stream.writeObject(dataObject.getLookup().lookup(DataModel.class));
+            } finally {
+                stream.close();
+            }
+        }
+    }
+
+    
 }
