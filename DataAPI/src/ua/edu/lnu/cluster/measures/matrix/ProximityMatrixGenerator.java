9ab3841005c6e72e0b84d8aa14bfcddd6fd1b042
Ostap <pifostap@gmail.com>
2011-05-12 03:06:06 +0300

    DataModel/Proximity improvements

--- DataAPI/src/ua/edu/lnu/cluster/measures/matrix/ProximityMatrixGenerator.java
+++ DataAPI/src/ua/edu/lnu/cluster/measures/matrix/ProximityMatrixGenerator.java
@@ -4,6 +4,7 @@
  */
 package ua.edu.lnu.cluster.measures.matrix;
 
+import java.util.List;
 import org.openide.util.lookup.ServiceProvider;
 import ua.edu.lnu.cluster.DataModel;
 import ua.edu.lnu.cluster.measures.api.ProximityMatrix;
@@ -21,10 +22,11 @@ public class ProximityMatrixGenerator implements ProximityMatrix {
         int size = model.getObservationCount();
         double[][] res = new double[size][size];
 
+        List<double[]> observations = model.getPreparedCalculationData();
         for (int i = 0; i < size; i++) {
-            double[] element1 = model.getObservationNormalized(i);
+            double[] element1 = observations.get(i);
             for (int j = i + 1; j < size; j++) {
-                res[i][j] = measure.distance(element1, model.getObservationNormalized(j));
+                res[i][j] = measure.distance(element1, observations.get(j));
             }
         }
 
