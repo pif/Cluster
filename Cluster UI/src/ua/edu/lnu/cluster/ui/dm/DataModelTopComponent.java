475beb59c504a1fa9a0ac6618a74ef2e6f33c64b
Ostap <pifostap@gmail.com>
2011-05-21 13:51:29 +0300

    problem was in wrong input file

--- Cluster UI/src/ua/edu/lnu/cluster/ui/dm/DataModelTopComponent.java
+++ Cluster UI/src/ua/edu/lnu/cluster/ui/dm/DataModelTopComponent.java
@@ -80,8 +80,8 @@ public final class DataModelTopComponent extends TopComponent {
 
         jTable1.setModel(new DMTableModel(dataModel));
         jTable1.getSelectionModel().addListSelectionListener(new DataColumnListener());
-        //jTable1.setDefaultRenderer(Object.class, new ValidCellRenderer());
-        //jTable1.setDefaultEditor(Integer.class, new CategoryCellEditor());
+        jTable1.setDefaultRenderer(Object.class, new ValidCellRenderer());
+        jTable1.setDefaultEditor(Integer.class, new CategoryCellEditor());
     }
 
     /** This method is called from within the constructor to
@@ -94,7 +94,6 @@ public final class DataModelTopComponent extends TopComponent {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -109,39 +108,25 @@ public final class DataModelTopComponent extends TopComponent {
         ));
         jScrollPane1.setViewportView(jTable1);
 
-        org.openide.awt.Mnemonics.setLocalizedText(jButton1, org.openide.util.NbBundle.getMessage(DataModelTopComponent.class, "DataModelTopComponent.jButton1.text")); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
-                    .addComponent(jButton1))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 276, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    }//GEN-LAST:event_jButton1ActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
